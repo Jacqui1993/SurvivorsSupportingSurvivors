54144214f90c9d67cc63bb10796ae9dc080d5f6c
Jacqui1993 <92681806+Jacqui1993@users.noreply.github.com>
2022-03-24 15:18:45 +0000

    Add files via upload

---
+++ ContactForm.php
@@ -0,0 +1,18 @@
+<?php
+
+if (isset($_POST['submit'])){
+    $name = $_POST['name'];
+    $subject = $_POST['subject'];
+    $mailFrom = $_POST['mail'];
+    $message = $_POST['message'];
+
+$mailTo = "jacqui2302@hotmail.com";
+$headers = "From: ".$mailFrom;
+$txt = "You hace received an e-mail from ".$name.".\n\n".$message;
+
+    mail($mailTo, $subject, $txt, $headers);
+    header("Location: ContactUs.html?mailsend");
+}
+
+
+?>
